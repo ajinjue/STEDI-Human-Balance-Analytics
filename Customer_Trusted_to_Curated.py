432cff4c61249c14a67ca07ed80141d548f91423
AKWAYAGA  COLLINUS AJINJUE <collinusakwayaga@gmail.com>
2023-09-03 10:20:19 +0100

    Sanitized Customer Trusted to Curated

---
+++ Customer_Trusted_to_Curated.py
@@ -0,0 +1,57 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Accelerometer Trusted
+AccelerometerTrusted_node1693730830466 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi_analytics",
+    table_name="accelerometer_trusted",
+    transformation_ctx="AccelerometerTrusted_node1693730830466",
+)
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi_analytics",
+    table_name="customer_trusted",
+    transformation_ctx="CustomerTrusted_node1",
+)
+
+# Script generated for node Join
+Join_node1693730888490 = Join.apply(
+    frame1=CustomerTrusted_node1,
+    frame2=AccelerometerTrusted_node1693730830466,
+    keys1=["email"],
+    keys2=["user"],
+    transformation_ctx="Join_node1693730888490",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1693730911817 = DropFields.apply(
+    frame=Join_node1693730888490,
+    paths=["timestamp", "user", "x", "y", "z"],
+    transformation_ctx="DropFields_node1693730911817",
+)
+
+# Script generated for node Customers Curated
+CustomersCurated_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1693730911817,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://akwa-bucket/customer/curated/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="CustomersCurated_node3",
+)
+
+job.commit()
